a27c6795cae398f5ac3a43446d44ba22825a6ced
Sarah Rogers <s.rogers0715@yahoo.com>
2020-07-14 01:53:27 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,8 @@
-require 'pry'
 def my_each(words)
   i=0
- while i< words.length 
- yield (words|i|)
-  i=i+1
-end
- words
+  while i< words.length 
+   yield (words|i|)
+    i=i+1
+  end
+  words
 end 
